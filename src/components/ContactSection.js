fa4b9c0e374f11e91363797fc30ae0ffa04893c9
oliverjam <oliverjamesphillips@gmail.com>
2017-11-01 17:35:45 +0000

    Hook up contact form

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -1,27 +1,70 @@
 import React from 'react';
+import fetch from 'unfetch';
+import { withStateHandlers } from 'recompose';
+
 import Title from './Title';
 import Card from './Card';
 import { Form, FormRow, InputWrapper, Label, Input, TextArea } from './Form';
 import Button from './Button';
 
-export default ({ title, label }) => (
+const ContactSection = ({
+  title,
+  label,
+  name,
+  email,
+  body,
+  loading,
+  handleChange,
+  setLoading,
+  setEmpty,
+}) => (
   <section aria-label={!title && label}>
     {title && <Title>{title}</Title>}
     <Card flexDirection={'column'} mx={'auto'} px={[2, 3]} py={3}>
-      <Form>
+      <Form
+        onSubmit={e => {
+          e.preventDefault();
+          setLoading();
+          submitData(e, name, email, body).then(response => {
+            console.log(response);
+            setEmpty();
+          });
+        }}
+      >
         <FormRow>
           <InputWrapper>
-            <Input name="name" id="name" type="text" required />
+            <Input
+              name="name"
+              id="name"
+              type="text"
+              required
+              value={name}
+              onChange={e => handleChange(e)}
+            />
             <Label for="name">Name</Label>
           </InputWrapper>
           <InputWrapper>
-            <Input name="email" id="email" type="text" required />
+            <Input
+              name="email"
+              id="email"
+              type="text"
+              required
+              value={email}
+              onChange={e => handleChange(e)}
+            />
             <Label for="email">Email</Label>
           </InputWrapper>
         </FormRow>
         <InputWrapper>
-          <TextArea name="email" id="email" rows="5" required />
-          <Label for="email">Your message</Label>
+          <TextArea
+            name="body"
+            id="body"
+            rows="5"
+            required
+            value={body}
+            onChange={e => handleChange(e)}
+          />
+          <Label for="body">Your message</Label>
         </InputWrapper>
         <Button
           width={[1, 'auto']}
@@ -30,9 +73,44 @@ export default ({ title, label }) => (
           py={1}
           type="submit"
         >
-          Submit
+          {loading ? 'Loading' : 'Submit'}
         </Button>
       </Form>
     </Card>
   </section>
 );
+
+const submitData = (e, name, email, body) => {
+  console.log('submitting...');
+  const data = {
+    name: name,
+    replyTo: email,
+    body: body,
+  };
+  return fetch('https://micro-mailer.now.sh', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    mode: 'cors',
+    body: JSON.stringify(data),
+  }).then(res => {
+    return res;
+  });
+};
+
+const withForm = withStateHandlers(
+  ({ name = '', email = '', body = '', loading = false }) => ({
+    name,
+    email,
+    body,
+    loading,
+  }),
+  {
+    handleChange: state => e => ({ [e.target.name]: e.target.value }),
+    setEmpty: state => () => ({ name: '', email: '', body: '' }),
+    setLoading: ({ loading }) => () => ({ loading: true }),
+  }
+);
+
+export default withForm(ContactSection);
